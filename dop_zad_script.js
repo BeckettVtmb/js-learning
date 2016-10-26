016c9d24425ed64f52d53f879229e7c2288237ba
BeckettVtmb <beckett.nook@gmail.com>
2016-10-26 20:11:07 +1000

    Create dop_zad_script.js

---
+++ dop_zad_script.js
@@ -0,0 +1,10 @@
+"use strict";
+var a = "Fizz", b = "Buzz", i = 1;
+
+while (i!=101) {
+	if ((i%5 != 0) & (i%3 != 0)) console.log(i);
+	else if ((i%5 == 0) & (i%3 == 0)) console.log(a+b);
+	else if ((i%3 == 0) & (i%5 != 0)) console.log(a);
+	else console.log(b);
+	++i; 
+}
