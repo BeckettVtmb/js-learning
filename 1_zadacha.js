c6057aee603e6eb26957cbac4bc1852f49e09414
BeckettVtmb <beckett.nook@gmail.com>
2016-11-02 18:37:36 +1000

    Update 1_zadacha.js

--- 1_zadacha.js
+++ 1_zadacha.js
@@ -1,7 +1,9 @@
 "use strict"; // Задание 1 к 4 уроку
 function min(a, b) {
-	var result = (a < b) ? a : b;
-	return result;
+	if (a != b) {
+		var result = (a < b) ? a : b;
+		return result;
+	}
 }
 console.log(min(0, 10));
 console.log(min(0, -10));
