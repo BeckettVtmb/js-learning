b1e38de19d928c9d02b2a1a722d9abcd79a282cb
BeckettVtmb <beckett.nook@gmail.com>
2016-11-02 18:18:11 +1000

    Update 1_zadacha.js

--- 1_zadacha.js
+++ 1_zadacha.js
@@ -1,10 +1,7 @@
-"use strict";
-var a = "Fizz", b = "Buzz", i = 1;
-
-while (i!=101) {
-	if ((i%5 != 0) && (i%3 != 0)) console.log(i);
-	else if ((i%5 == 0) && (i%3 == 0)) console.log(a+b);
-	else if ((i%3 == 0) && (i%5 != 0)) console.log(a);
-	else console.log(b);
-	++i; 
+"use strict"; // Задание 1 к 4 уроку
+function min(a, b) {
+	var result = (a < b) ? a : b;
+	return result;
 }
+console.log(min(0, 10));
+console.log(min(0, -10));
