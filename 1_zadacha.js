2cd8c80b2a258fb2e6948d355b39e449a298c01a
BeckettVtmb <beckett.nook@gmail.com>
2016-11-05 23:17:26 +1000

    Update 1_zadacha.js

--- 1_zadacha.js
+++ 1_zadacha.js
@@ -1,9 +1,22 @@
-"use strict"; // Задание 1 к 4 уроку
-function min(a, b) {
-	if (a != b) {
-		var result = (a < b) ? a : b;
-		return result;
+"use strict";
+function tTe(a, b, c) {
+	var func = {};
+	func.boofer = [a, b, c];
+	func.main = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A" ,"B" ,"C" ,"D" ,"E" ,"F"];
+	func.result = [0, 0];
+	func.hex = "";
+	for (var i = 0; i < 3; ++i) {
+			for (var j = 2; j != 0; --j) {			
+				func.result[j-1] = func.main[func.boofer[i] % 16];
+				func.boofer[i] = ~~(func.boofer[i] / 16);
+			}
+			for (var k = 0; k < 2; ++k) {
+				func.hex += func.result[k];
+			}
 	}
+	return func.hex;
 }
-console.log(min(0, 10));
-console.log(min(0, -10));
+console.log (tTe(145, 168, 245));
+console.log (tTe(255, 0, 255));
+console.log (tTe(255, 255, 0));
+console.log (tTe(0, 255, 255));
