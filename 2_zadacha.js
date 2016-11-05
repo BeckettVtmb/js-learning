6a62bb09d10a7bc734dd2041b3283361f65b724c
BeckettVtmb <beckett.nook@gmail.com>
2016-11-06 00:17:11 +1000

    Update 2_zadacha.js

--- 2_zadacha.js
+++ 2_zadacha.js
@@ -1,6 +1,10 @@
 "use strict";
 function numberToObject(a) {
 	var result = {};
+	if ((a  >  999) || (a < 0) || (typeof(a) != "number")) {
+		console.log ("Неверный диапазон числа или тип параметра!");
+		return result;
+	}
 	result.единицы = 0;
 	result.десятки = 0;
 	result.сотни = 0;
@@ -15,3 +19,6 @@ console.log (numberToObject(999));
 console.log (numberToObject(0));
 console.log (numberToObject(2));
 console.log (numberToObject(35));
+console.log (numberToObject("Test"));
+console.log (numberToObject(1000));
+console.log (numberToObject(-267));
